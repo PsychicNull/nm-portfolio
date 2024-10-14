82894a7298782526a7fe4185fb031ac035ab76f0
PsychicNull <wfranz.lee@gmail.com>
2024-10-14 18:41:25 -0500

    Update custom-cursors.js

--- js/custom-cursors.js
+++ js/custom-cursors.js
@@ -3,7 +3,7 @@
 $(document).ready(function() {
     $('.vertical-swiper .swiper-button-prev').hover(
         function() { // On mouse enter
-            $(this).css('cursor', 'url(../img/cursor/cursor_glove_left_32_01.png) 10 10, auto');
+            $(this).css('cursor', 'url(img/cursor/cursor_glove_left_32_01.png) 10 10, auto');
         }, 
         function() { // On mouse leave
             $(this).css('cursor', 'auto'); // Reset cursor
@@ -12,7 +12,7 @@ $(document).ready(function() {
 
     $('.vertical-swiper .swiper-button-next').hover(
         function() { // On mouse enter
-            $(this).css('cursor', 'url(../img/cursor/cursor_glove_right_32_01.png) 10 10, auto');
+            $(this).css('cursor', 'url(img/cursor/cursor_glove_right_32_01.png) 10 10, auto');
         }, 
         function() { // On mouse leave
             $(this).css('cursor', 'auto'); // Reset cursor
@@ -21,7 +21,7 @@ $(document).ready(function() {
     
     $('.horizontal-swiper .swiper-button-prev').hover(
         function() { // On mouse enter
-            $(this).css('cursor', 'url(../img/cursor/cursor_glove_left_32_01.png) 10 10, auto');
+            $(this).css('cursor', 'url(img/cursor/cursor_glove_left_32_01.png) 10 10, auto');
         }, 
         function() { // On mouse leave
             $(this).css('cursor', 'auto'); // Reset cursor
@@ -30,7 +30,7 @@ $(document).ready(function() {
 
     $('.horizontal-swiper .swiper-button-next').hover(
         function() { // On mouse enter
-            $(this).css('cursor', 'url(../img/cursor/cursor_glove_right_32_01.png) 10 10, auto');
+            $(this).css('cursor', 'url(img/cursor/cursor_glove_right_32_01.png) 10 10, auto');
         }, 
         function() { // On mouse leave
             $(this).css('cursor', 'auto'); // Reset cursor
@@ -39,7 +39,7 @@ $(document).ready(function() {
     
     $('.side-tab').hover(
         function() { // On mouse enter
-            $(this).css('cursor', 'url(../img/cursor/cursor_glove.png), auto');
+            $(this).css('cursor', 'url(img/cursor/cursor_glove.png), auto');
         }, 
         function() { // On mouse leave
             $(this).css('cursor', 'auto'); // Reset cursor
@@ -49,7 +49,7 @@ $(document).ready(function() {
     // Apply custom cursor for links and specific elements
     $('a, .dropdown-toggle, .hamburger-menu').hover(
         function() { // On mouse enter
-            $(this).css('cursor', 'url(../img/cursor/cursor_glove.png) 10 10, auto');
+            $(this).css('cursor', 'url(img/cursor/cursor_glove.png) 10 10, auto');
         }, 
         function() { // On mouse leave
             $(this).css('cursor', 'auto'); // Reset cursor
@@ -59,7 +59,7 @@ $(document).ready(function() {
     // Optional: Apply custom cursor for the entire body only when no other element is hovered
     $('body').hover(
         function() { // On mouse enter
-            $(this).css('cursor', 'url(../img/cursor/cursor_glove.png) 10 10, auto');
+            $(this).css('cursor', 'url(img/cursor/cursor_glove.png) 10 10, auto');
         }, 
         function() { // On mouse leave
             $(this).css('cursor', 'auto'); // Reset cursor
