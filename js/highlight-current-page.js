570e3919e9a2b99f3a6a8f0618a2b11775692e6f
PsychicNull <wfranz.lee@gmail.com>
2024-11-14 16:40:27 -0600

    Minor changes

--- js/highlight-current-page.js
+++ js/highlight-current-page.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
     var navItems = {
         '/index.html': '#nav-list li:eq(0) a', // Works page (index.html)
         '/html/context.html': '#nav-list li:eq(1) a', // Context page
-        // Add more paths if needed
+        '/html/contact.html': '#nav-list li:eq(2) a', // Contact page
     };
 
     // Function to set the active class
@@ -30,7 +30,7 @@ $(document).ready(function() {
     setActiveClass(currentUrl);
     
     // Handle click event on nav links
-    $('#nav-list a').on('click', function(event) {
+    $('#nav-list a, #nav-list-mobile a').on('click', function(event) {
         event.preventDefault(); // Prevent default link behavior
         var href = $(this).attr('href').replace(/\/$/, ''); // Normalize href by removing trailing slash
         
@@ -44,45 +44,3 @@ $(document).ready(function() {
     });
 });
 
-
-
-
-// $(document).ready(function() {
-//     // Get the current URL
-//     var currentUrl = window.location.pathname;
-
-//     // Define the mapping of normalized URLs to the navigation items
-//     var navItems = {
-//         '/index.html': '#nav-list li:eq(0) a', // Works page (index.html)
-//         '/html/context.html': '#nav-list li:eq(1) a', // Context page
-//         // '/html/about.html': '#nav-list li:eq(2) a', // About page
-//     };
-
-//     // Function to set the active class
-//     window.setActiveClass = function(url) {  // Make it a global function
-//         url = url.replace(/\/$/, ''); // Remove trailing slash if present
-//         for (var page in navItems) {
-//             var link = $(navItems[page]);
-//             if (url === page) {
-//                 link.addClass('active'); // Highlight active link
-//                 // link.css('background-size', '100% 100%'); // Ensure background grows
-//             } else {
-//                 link.removeClass('active'); // Remove active class from others
-//             }
-//         }
-//     };
-
-//     // Set the active class on load
-//     setActiveClass(currentUrl);
-    
-//     // Handle click event on nav links
-//     $('#nav-list a').on('click', function(event) {
-//         var href = $(this).attr('href');
-//         setActiveClass(href); // Update active class on click
-
-//         // Optional: Add a short delay before navigation
-//         setTimeout(function() {
-//             window.location.href = href; // Navigate to the link
-//         }, 100); // Adjust timing as needed
-//     });
-// });
